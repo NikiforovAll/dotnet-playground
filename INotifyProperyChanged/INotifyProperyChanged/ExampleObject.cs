3ac68ff416820505cb923886bda754d78d94856e
NikiforovAll <nik-alekcey@yandex.ua>
2017-02-01 01:34:49 +0200

    INotifyPropertyChanged

---
+++ INotifyProperyChanged/INotifyProperyChanged/ExampleObject.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace INotifyProperyChangedDemo
+{
+    public interface IFormatter<T>
+    {
+        T FormatField(T source);
+    }
+
+    public class ValueToUpperCaseFormatter : IFormatter<string>
+    {
+        public string FormatField(string source)
+        {
+            if (source == null) throw new ArgumentNullException("Invalid value for formatter");
+            return source.ToUpper();
+        }
+    }
+
+    public class DefaultFormatter<T> : IFormatter<T>
+    {
+        public T FormatField(T source) => source;
+    }
+
+    public class CustomPropertyChangedEventArgs : PropertyChangedEventArgs
+    {
+        public string ModifiedField { get; set; }
+
+        public CustomPropertyChangedEventArgs(string propertyName) : base(propertyName)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Field changed: {PropertyName}\nFormatter: {ModifiedField}";
+        }
+    }
+
+    class ExampleObject<TFormatter> : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null, string modifiedValue = "default")
+        {
+            PropertyChanged?.Invoke(this,
+                new CustomPropertyChangedEventArgs(propertyName) { ModifiedField = modifiedValue });
+        }
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            var formatter = new ValueToUpperCaseFormatter();
+            OnPropertyChanged(propertyName, formatter.FormatField(value as string));
+            return true;
+        }
+
+        public ExampleObject()
+        {
+            _data = "default value";
+        }
+
+        private string _data;
+        public string Data
+        {
+            get { return _data; }
+            set { SetField(ref _data, value, nameof(Data)); }
+        }
+    }
+}
---
+++ INotifyProperyChanged/INotifyProperyChanged/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INotifyProperyChangedDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var object1 = new ExampleObject<ValueToUpperCaseFormatter>();
+            object1.PropertyChanged += (sender, arg) => {
+                Console.WriteLine(arg); };
+            object1.Data = "changed";
+        }
+    }
+}
