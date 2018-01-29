da5491bcefb6fed1e62defa0c9934d215076464f
NikiforovAll <nik-alekcey@yandex.ua>
2018-01-30 00:06:43 +0200

    added custom server implementation of IServer interface from Microsoft.AspNetCore.Hosting namespace

---
+++ IServer/CustomServerFolderWatcher.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http.Features;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+namespace CustomServer
+{
+    public class CustomServerFolderWatcher<TContext>
+    {
+        private readonly FileSystemWatcher watcher;
+        private readonly IHttpApplication<TContext> application;
+        private readonly IFeatureCollection features;
+        public CustomServerFolderWatcher(IHttpApplication<TContext> application, IFeatureCollection features)
+        {
+            var path = features.Get<IServerAddressesFeature>().Addresses.FirstOrDefault();
+            this.watcher = new FileSystemWatcher(path);
+            this.watcher.EnableRaisingEvents = true;
+            this.application = application;
+            this.features = features;
+        }
+        public void Watch()
+        {
+            watcher.Created += async (sender, e) =>
+            {
+                var context = (HostingApplication.Context)(object)application.CreateContext(features);
+
+                context.HttpContext = new CustomHttpContext(features, e.FullPath);
+
+                await application.ProcessRequestAsync((TContext)(object)context);
+
+                context.HttpContext.Response.OnCompleted(null, null);
+            };
+
+            Task.Run(() => watcher.WaitForChanged(WatcherChangeTypes.All));
+        }
+    }
+}
